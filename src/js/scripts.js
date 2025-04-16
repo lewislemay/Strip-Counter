02ba380c0e0353aa3e3314fb311afc1679d67538
Lewis Lemay <lalemay@ualberta.ca>
2025-04-15 19:29:20 -0600

    Add complete dictionary of strip sets

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -11,14 +11,52 @@ const lastUpdate = "April 11, 2025"
 
 //Define dictionary -- for keeping total amounts
 const allBrands = {
-    "OLIGO Calura Permanent": 194,
-    "OLIGO Calura Gloss": 96,
-    "OLIGO Caluraten": 52,
-    "IGORA Zero Amm": 171,
-    "CELEB": 125,
-    "MOROCCANOIL Color Rhapsody 10 Minute": 125,
+    "CELEB Luxury": 125,
+    "CHI Ionic": 0,
+    "DANGER JONES Semi's": 0,
+    "DANGER JONES Toners": 0,
+    "JOICO Vero K-PAK": 0,
+    "JOICO Vero K-PAK Age Defy": 0,
+    "JOICO Lumishine": 0,
+    "JOICO Lumishine Youthlock": 0,
+    "JOICO Lumishine DD": 0,
+    "JOICO Lumishine Liquid": 0,
+    "JOICO Lumi 10": 0,
+    "JOICO Color Intensity": 124,
+    "KENRA Permanent": 0,
+    "KENRA Monochrome": 0,
+    "KENRA Demi": 0,
+    "KENRA Express": 0,
+    "KEUNE Tinta": 0,
+    "KEUNE Semi": 0,
+    "LANZA Healing Color": 0,
+    "MOROCCANOIL Color Rhapsody": 0,
     "MOROCCANOIL Color Calypso": 80,
-    "JOICO Color Intensity": 124
+    "MOROCCANOIL Highlift & Color Infusion": 0,
+    "MOROCCANOIL Ultimates": 0,
+    "MOROCCANOIL Color Rhapsody 10 Minute": 125,
+    "NOOK The Origin Color": 0,
+    "OLIGO Calura Permanent": 194,
+    "OLIGO Calura Ten": 52,
+    "OLIGO Calura Gloss": 96,
+    "PRAVANA ChromaSilk Permanent": 0,
+    "PRAVANA ChromaSilk Express Tones": 0,
+    "PRAVANA ChromaSilk Vivids": 0,
+    "PRAVANA ChromaSilk Hydragloss": 0,
+    "PULPRIOT Semi & Toners": 0,
+    "SCHWARZKOPF IGORA Royal": 0,
+    "SCHWARZKOPF IGORA Color 10": 0,
+    "SCHWARZKOPF IGORA Absolutes": 0,
+    "SCHWARZKOPF IGORA Vibrance": 0,
+    "SCHWARZKOPF IGORA Highlift": 0,
+    "SCHWARZKOPF IGORA Zero AMM": 171,
+    "SCHWARZKOPF BLONDME": 0,
+    "WELLA Koleston Perfect": 0,
+    "WELLA Koleston Xpress": 0,
+    "WELLA Color Touch": 0,
+    "WELLA Illumina": 0,
+    "WELLA Shinefinity": 0,
+    "WELLA Blondor": 0
 }
 
 //Function -- adding counted strips
@@ -51,10 +89,33 @@ function calcRemainder() {
     return extraStrips;
 }
 
+//Function -- calculating strip total with total sets + extras
+async function calcStrips() {
+
+    //Get how many total sets
+    let countedSets = await input("How many sets? ");
+    let arraySets = countedSets.split(',');
+    totalCountedSets = 0;
+    for (sets of arraySets) {
+        let num2 = parseInt(sets.trim());
+        if (!isNaN(num2)) {
+            totalCountedSets += num2;
+        }
+    }
+
+    //Get how many strips per set
+    let countedExtras = await input("How many strips per set? ");
+    countedExtras = parseInt(countedExtras);
+    
+    //Get total strip number
+    totalCountedStrips = (totalCountedSets * countedExtras);
+    return totalCountedStrips;
+}
+
 //Function -- main
 async function main() {
     do {
-        output("Enter an option: \n(D)isplay \n(C)ount");
+        output("Enter an option: \n(D)isplay \n(C)ount \n(R)ecount");
         menuSelect = await input("");
         menuSelect = menuSelect.toLowerCase();
     
@@ -67,8 +128,6 @@ async function main() {
                 for (const [key, value] of Object.entries(allBrands)) {
                     output(`${key}: ${value}`);
                 }
-                askAgain = await input("Return to menu?(Y/N) ");
-                askAgain = askAgain.toLowerCase();
                 break;
     
             //Menu -- count
@@ -77,8 +136,12 @@ async function main() {
                 await calcSets();
                 await calcRemainder();
                 output(`Total Strips: ${totalStrips} \nTotal Sets: ${totalSets} \nExtra Strips: ${extraStrips}`);
-                askAgain = await input("Return to menu?(Y/N) ");
-                askAgain = askAgain.toLowerCase();
+                break;
+
+            //Menu -- recount
+            case "r":
+                await calcStrips();
+                output(`Total Strips: ${totalCountedStrips}`);
                 break;
     
             //Default -- end session
@@ -86,5 +149,5 @@ async function main() {
                 output("Have a great day!");
                 break;
     }
-    } while (menuSelect.includes("d") || menuSelect.includes("c"));
+    } while (menuSelect.includes("d") || menuSelect.includes("c") || menuSelect.includes("r"));
 }
