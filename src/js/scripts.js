4162b6a8dc13d7a58f3562c6ad98f90c16a8d7c8
itsmoongoose <lewishlemay@gmail.com>
2025-07-26 19:28:21 -0600

    Remove inventory tracking

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,61 +6,6 @@ let stripsPerStore = 0;
 let askAgain = "";
 let menuSelect = "";
 
-/*
-//Date of last count/update
-const lastUpdate = "April 11, 2025"
-
-//Define dictionary -- for keeping total amounts
-const allBrands = {
-    "CELEB Luxury": 125,
-    "CHI Ionic": 0,
-    "DANGER JONES Semi's": 0,
-    "DANGER JONES Toners": 0,
-    "JOICO Vero K-PAK": 0,
-    "JOICO Vero K-PAK Age Defy": 0,
-    "JOICO Lumishine": 0,
-    "JOICO Lumishine Youthlock": 0,
-    "JOICO Lumishine DD": 0,
-    "JOICO Lumishine Liquid": 0,
-    "JOICO Lumi 10": 0,
-    "JOICO Color Intensity": 124,
-    "KENRA Permanent": 0,
-    "KENRA Monochrome": 0,
-    "KENRA Demi": 0,
-    "KENRA Express": 0,
-    "KEUNE Tinta": 0,
-    "KEUNE Semi": 0,
-    "LANZA Healing Color": 0,
-    "MOROCCANOIL Color Rhapsody": 0,
-    "MOROCCANOIL Color Calypso": 80,
-    "MOROCCANOIL Highlift & Color Infusion": 0,
-    "MOROCCANOIL Ultimates": 0,
-    "MOROCCANOIL Color Rhapsody 10 Minute": 125,
-    "NOOK The Origin Color": 0,
-    "OLIGO Calura Permanent": 194,
-    "OLIGO Calura Ten": 52,
-    "OLIGO Calura Gloss": 96,
-    "PRAVANA ChromaSilk Permanent": 0,
-    "PRAVANA ChromaSilk Express Tones": 0,
-    "PRAVANA ChromaSilk Vivids": 0,
-    "PRAVANA ChromaSilk Hydragloss": 0,
-    "PULPRIOT Semi & Toners": 0,
-    "SCHWARZKOPF IGORA Royal": 0,
-    "SCHWARZKOPF IGORA Color 10": 0,
-    "SCHWARZKOPF IGORA Absolutes": 0,
-    "SCHWARZKOPF IGORA Vibrance": 0,
-    "SCHWARZKOPF IGORA Highlift": 0,
-    "SCHWARZKOPF IGORA Zero AMM": 171,
-    "SCHWARZKOPF BLONDME": 0,
-    "WELLA Koleston Perfect": 0,
-    "WELLA Koleston Xpress": 0,
-    "WELLA Color Touch": 0,
-    "WELLA Illumina": 0,
-    "WELLA Shinefinity": 0,
-    "WELLA Blondor": 0
-}
-*/
-
 //Function -- adding counted strips
 async function addStrips() {
     let stripCount = await input("How many strips: ");
